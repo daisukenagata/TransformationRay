b85ec757042dcb811d443611067798b5cac05647
永田大祐 <dbank0208@gmail.com>
2017-04-21 04:30:30 +0900

    change

--- TransformationRay/VC/ViewController.swift
+++ TransformationRay/VC/ViewController.swift
@@ -14,18 +14,18 @@ class ViewController: UIViewController,UIGestureRecognizerDelegate  {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       CommonStructure.tapGesture = UITapGestureRecognizer(target: self, action: #selector(singleTap))
-         CommonStructure.tapGesture.delegate = self
+        CommonStructure.tapGesture = UITapGestureRecognizer(target: self, action: #selector(singleTap))
+        CommonStructure.tapGesture.delegate = self
         self.view.addGestureRecognizer( CommonStructure.tapGesture)
         
-         CommonStructure.swipePinchGesture = UIPinchGestureRecognizer(target: self, action:#selector(zoomAction(sender:)))
+        CommonStructure.tapLong = UILongPressGestureRecognizer(target: self, action:#selector(longTappled))
+        self.view.addGestureRecognizer( CommonStructure.tapLong)
+        
+        CommonStructure.swipePinchGesture = UIPinchGestureRecognizer(target: self, action:#selector(zoomAction(sender:)))
         self.view.addGestureRecognizer( CommonStructure.swipePinchGesture)
         
-         CommonStructure.swipePanGesture = UIPanGestureRecognizer(target: self, action:#selector(panLabel(sender:)))
+        CommonStructure.swipePanGesture = UIPanGestureRecognizer(target: self, action:#selector(panLabel(sender:)))
         self.view.addGestureRecognizer( CommonStructure.swipePanGesture)
-        
-         CommonStructure.tapLong = UILongPressGestureRecognizer(target: self, action:#selector(longTappled(sender:)))
-        self.view.addGestureRecognizer( CommonStructure.tapLong)
         
     }
     
@@ -56,7 +56,51 @@ class ViewController: UIViewController,UIGestureRecognizerDelegate  {
     
     func longTappled(sender: UILongPressGestureRecognizer) {
         
-         CommonStructure.line.removeAllPoints()
+        CommonStructure.line.removeAllPoints()
+        
+    }
+    
+    @IBAction func actionBool(_ sender: UIBarButtonItem){
+        
+        if GestureField.bool == false {
+            
+            GestureField.bool = true
+            
+        } else if GestureField.bool == true {
+            
+            GestureField.bool = false
+            
+        }
+        
+    }
+    
+    @IBAction func actionButton(_ sender: UIBarButtonItem) {
+        
+        if  CommonStructure.count.count > 2{
+            CommonStructure.line.move(to: CGPoint(x:  CommonStructure.count[ CommonStructure.count.endIndex-2].x,y:CommonStructure.count[ CommonStructure.count.endIndex-2].y))
+            CommonStructure.line.addLine(to:CGPoint(x:  CommonStructure.count[CommonStructure.count.endIndex-1].x , y: CommonStructure.count[ CommonStructure.count.endIndex-1].y))
+            self.view.addSubview(labelSet(view:  CommonStructure.views))
+        }
+        
+    }
+    
+    @IBAction func cirle(_ sender: UIBarButtonItem) {
+        
+        CommonStructure.line = UIBezierPath(roundedRect:CommonStructure.line.bounds, cornerRadius:CommonStructure.line.bounds.width / 2)
+        self.view.addSubview(labelSet(view: CommonStructure.views))
+        
+    }
+    
+    @IBAction func removeSegue(_ sender: UIBarButtonItem) {
+        
+        GestureField.bool = false
+        LineField.lineField(line:CommonStructure.line, viewC: self)
+        
+    }
+    
+    @IBAction func transformImage(_ sender: UIBarButtonItem) {
+        
+        ConversionImage().toImage(viewC: self,uiImage:CommonStructure.vw)
         
     }
     
@@ -66,61 +110,16 @@ class ViewController: UIViewController,UIGestureRecognizerDelegate  {
         
     }
     
-    
-    @IBAction func actionBool(_ sender: UIBarButtonItem){
+    @IBAction func slender(_ sender: UIBarButtonItem) {
         
-        if GestureField.bool == false {
-            
-        GestureField.bool = true
-            
-        } else if GestureField.bool == true {
-            
-        GestureField.bool = false
-            
-        }
+        CommonStructure.lineWidth -= 1
         
-    }
-
-    @IBAction func actionButton(_ sender: UIBarButtonItem) {
-        
-        if  CommonStructure.count.count > 2{
-             CommonStructure.line.move(to: CGPoint(x:  CommonStructure.count[ CommonStructure.count.endIndex-2].x,y:CommonStructure.count[ CommonStructure.count.endIndex-2].y))
-             CommonStructure.line.addLine(to:CGPoint(x:  CommonStructure.count[CommonStructure.count.endIndex-1].x , y: CommonStructure.count[ CommonStructure.count.endIndex-1].y))
-             self.view.addSubview(labelSet(view:  CommonStructure.views))
-        }
-        
-    }
-    
-    @IBAction func cirle(_ sender: UIBarButtonItem) {
-        
-         CommonStructure.line = UIBezierPath(roundedRect:CommonStructure.line.bounds, cornerRadius:CommonStructure.line.bounds.width / 2)
-        self.view.addSubview(labelSet(view: CommonStructure.views))
-        
-    }
-    
-    @IBAction func removeSegue(_ sender: UIBarButtonItem) {
-        
-        GestureField.bool = false
-        LineField.lineField(line:CommonStructure.line, viewC: self)
-
     }
     
     @IBAction func boldly(_ sender: UIBarButtonItem) {
         
-         CommonStructure.lineWidth += 1
+        CommonStructure.lineWidth += 1
         
     }
     
-    @IBAction func slender(_ sender: UIBarButtonItem) {
-        
-         CommonStructure.lineWidth -= 1
-        
-    }
-    
-    @IBAction func transformImage(_ sender: UIBarButtonItem) {
-        
-        ConversionImage().toImage(viewC: self,uiImage:CommonStructure.vw)
-        
-    }
 }
-
