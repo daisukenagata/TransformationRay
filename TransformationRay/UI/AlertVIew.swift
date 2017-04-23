e580c3c5c57e52cc322a455aa9d71d013d9601c3
永田大祐 <dbank0208@gmail.com>
2017-04-23 15:16:09 +0900

    Code simplified

--- TransformationRay/UI/AlertVIew.swift
+++ TransformationRay/UI/AlertVIew.swift
@@ -9,17 +9,7 @@
 import UIKit
 
 
-class AlertView:UIView{
-    
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+class AlertView {
     
     
     func Alert(view:UIViewController)->String{
